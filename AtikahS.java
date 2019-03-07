5494f0178e8964d38a428f359daa86d47c8ba0df
AtikahSylviana <48308151+AtikahSylviana@users.noreply.github.com>
2019-03-07 12:52:09 +0700

    Add files via upload

---
+++ AtikahS.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Atikah;
+
+public class AtikahS  {
+    public static void main(String[] args) {
+         System.out.println("Nama : Atikah Sylviana \n");
+         System.out.println("Nim  : 17090141\n");
+         System.out.println("Kelas : 4A \n");
+    }
+    
+}
